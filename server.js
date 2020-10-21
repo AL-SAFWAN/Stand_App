3e34c6ec9f7393c34fd3ea9fa56ea31dfb254dbc
= <=>
2020-10-21 05:00:34 +0100

    img upload and slack

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const io = socketIo(server)
 
 const models = require("./models");
 const connection = require("./db");
-
-
+const { request } = require("express");
 
 
 // used for pulling data from the backend
@@ -23,22 +22,22 @@ app.use("/api/notes", require("./routes/api/notes"));
 app.use("/api/standupDates", require("./routes/api/standupDates"));
 app.use("/api/supportDates", require("./routes/api/supportDates"));
 
-// upload end point 
-app.post('/upload', (req,res) =>{
-    if(req.files === null) {
-        return res.status(400).json({msg: "no image uploaded "})
+// Image upload end point 
+app.post('/upload', (req, res) => {
+    if (req.files === null) {
+        return res.status(400).json({ msg: "no image uploaded " })
     }
     const file = req.files.file
-    console.log("in file call",file)
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err=>{
-        if(err){
+    console.log("in file call", file)
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if (err) {
             console.log(err)
             return res.status(500).send(err)
         }
-        res.json({fileName: file.name,filePath : `/uploads/${file.name}`})
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` })
     })
 })
-
+// socket end point 
 const users = []
 const result = []
 
@@ -64,7 +63,6 @@ io.on("connection", socket => {
 
                 })
 
-
         } else {
             console.log("---------------------\n")
             console.log("the same user has connected ")
@@ -72,73 +70,51 @@ io.on("connection", socket => {
             io.emit("users", result)
         }
         // need a logout? to remove the user
-
-
-
     })
 
 
     socket.on("getAllUser", (cb) => {
-
         connection.query("SELECT name,id,filePath from standapp.users")
             .then(([results, metadata]) => {
                 console.log(results, "<-----results of select name ------\n,")
                 const users = [...results]
-                const newArray =[]
+                const newArray = []
                 users.forEach((user, i) => {
                     connection.query(`select sum(isCompleted =1)as done,sum(isCompleted =0)as notDone from standapp.items where userId = ${user.id} and date(createdAt) = current_date()`)
                         .then(([results, metadata]) => {
-                        
-                            newArray.push({name: user.name, filePath:user.filePath, result : results[0] })
-                         
-                        if((users.length-1) === i ){
-                            console.log( "new array ->",newArray)
-                              cb(newArray)
-
+                            newArray.push({ name: user.name, filePath: user.filePath, result: results[0] })
+                            if ((users.length - 1) === i) {
+                                console.log("new array ->", newArray)
+                                cb(newArray)
+                            }
                         }
-                        }
-
                         )
-                        
-
                 })
-                
-              
-
             })
     })
 
-    
     socket.on("getAllActiveUser", (cb) => {
-
         connection.query(" SELECT users.id,users.name,users.filePath FROM standapp.items  join standapp.users on items.userId = users.id where date(items.createdAt) = current_date() group by users.name")
             .then(([results, metadata]) => {
-
                 console.log(results, "<-----results of select active name ------\n,")
 
                 const users = [...results]
-                const newArray =[]
+                const newArray = []
 
                 users.forEach((user, i) => {
                     connection.query(`select sum(isCompleted =1)as done,sum(isCompleted =0)as notDone from standapp.items where userId = ${user.id} and date(createdAt) = current_date()`)
                         .then(([results, metadata]) => {
-                        
-                            newArray.push({name: user.name,filePath:user.filePath, result : results[0] })
-                         
-                        if((users.length-1) === i ){
-                            console.log( "new array ->",newArray)
-                              cb(newArray)
 
-                        }
-                        }
+                            newArray.push({ name: user.name, filePath: user.filePath, result: results[0] })
 
+                            if ((users.length - 1) === i) {
+                                console.log("new array ->", newArray)
+                                cb(newArray)
+
+                            }
+                        }
                         )
-                        
-
                 })
-                
-              
-
             })
     })
 
@@ -161,10 +137,6 @@ io.on("connection", socket => {
         io.emit("users", result)
 
     })
-
-    
-
-
     socket.on("disconnect", () => {
         console.log("-----SOCKET------\n")
         console.log("User SOCKET has left")
@@ -178,6 +150,34 @@ io.on("connection", socket => {
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log("server started on port :" + port))
+
+
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+// env file is not working for me ?? 
+// need to look into it more 
+const appy = new App({
+    token: "xoxb-4249473565-1436504183284-5YvkBmPc5AT6IxztFd7j84iD",
+    signingSecret: "df8904bd1b81faa5bcee6db100d6ef8a"
+});
+
+
+
+// All the room in the world for your code
+appy.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hey there <@${message.user}>!`);
+  });
+  
+
+
+(async () => {
+    // Start your app
+    await appy.start(process.env.PORT || 3030);
+
+    console.log('⚡️ Bolt app is running on 3030! ');
+})();
+
 
 
 // === this will load all the users will there name 
---
+++ slack.js
@@ -0,0 +1,21 @@
+// // Require the Bolt package (github.com/slackapi/bolt)
+// const { App } = require("@slack/bolt");
+// import {SLACK_BOT_TOKEN,SLACK_SIGNING_SECRET} from './const'
+
+// const app = new App({
+//   token: SLACK_BOT_TOKEN,
+//   signingSecret: SLACK_SIGNING_SECRET
+// });
+
+
+
+// // All the room in the world for your code
+
+
+
+// (async () => {
+//   // Start your app
+//   await app.start(process.env.PORT || 3000);
+
+//   console.log('⚡️ Bolt app is running!');
+// })();
