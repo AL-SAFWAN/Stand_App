fd6b65b70090e4458fb724fa003dd6911681f9d5
= <=>
2020-10-25 03:24:13 +0000

    using Redirect

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import StandupForm from './Components/StandupForm/page'
 import SupportPage from './Components/SupportPage'
 import AdminPage from './Components/AdminPage'
 
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
 
 
 
@@ -39,10 +39,10 @@ function App( ) {
       <Bar style={{ position: "sticky" }} state={state} dispatch={dispatch}></Bar>
       <Switch >
         <Route exact path='/' render={() => <OnLog state={state} />} />
-        <Route exact path='/support' render={() =>authenticated ? <SupportPage state={state} /> : <OnLog state={state} />} />
-        <Route exact path='/standup' render={() => authenticated ?<StandupForm state={state} /> : <OnLog state={state} />} />   
-        <Route exact path='/admin' render={() =>authenticated ?<AdminPage state={state} /> : <OnLog state={state} />} />
-        <Route exact path='/Setting' render={() =>authenticated? <Setting state={state} /> : <OnLog state={state} />} />
+        <Route exact path='/support' render={() =>authenticated ? <SupportPage state={state} /> : <Redirect to='/'/> } />
+        <Route exact path='/standup' render={() => authenticated ?<StandupForm state={state} /> : <Redirect to='/'/> } />   
+        <Route exact path='/admin' render={() =>authenticated ?<AdminPage state={state} /> :<Redirect to='/'/> } />
+        <Route exact path='/Setting' render={() =>authenticated? <Setting state={state} /> : <Redirect to='/'/> } />
 
       </Switch>
 
